3bd5b13922d8f133b781667bd7c79b5f0b225640
Kazuhiro Kaneki <kaazzzuu@gmail.com>
2017-04-25 05:58:28 +0000

    twitter clone

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   def index
     @tasks = Task.all.page(params[:page]).per(10)
   end
@@ -12,12 +13,14 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      redirect_to root_url
+      
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
@@ -36,6 +39,7 @@ class TasksController < ApplicationController
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
+
     end
   end
 
@@ -44,7 +48,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
@@ -54,5 +58,11 @@ class TasksController < ApplicationController
   end
   def task_params
     params.require(:task).permit(:content, :status)
-end
+  end
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_path
+    end
+  end
 end
